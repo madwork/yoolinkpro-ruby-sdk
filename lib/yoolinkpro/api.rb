08988b8ae1630e1eb1077f287251c947aa3d69fa
Vincent Durand <vincent.durand@madwork.org>
2013-01-22 13:07:01 +0100

    remove method_missing for new design, implement simple method find, update... with object as parameter

--- lib/yoolinkpro/api.rb
+++ lib/yoolinkpro/api.rb
@@ -5,6 +5,13 @@ require 'cgi'
 module Yoolinkpro
   class Api < SimpleDelegator
   
+    # Yoolinkpro::Api
+    #
+    #   client = Yoolinkpro::Client.new
+    #   api = Yoolinkpro::Api.new client
+    #
+    # @param [Yoolinkpro::Client] client
+    # @return [Yoolinkpro::Api]
     def initialize(client)
       super
       @millitime = Time.now.to_i*1000
@@ -12,37 +19,72 @@ module Yoolinkpro
       @key_scopes = { :private => private_key, :admin => admin_key }
     end
     
+    # Create an url to open a new session
+    #
+    #   api.open_session "vincent@yoolink.fr"
+    #
+    # @param [String] email of an existing YoolinkPro user
+    # @return [RestClient::Response]
     def open_session(email)
       @key = "get/open_sessionemail=#{email}#{admin_key}#{@millitime}#{@int}"
       RestClient::Response.create "https://#{Yoolinkpro::API_SERVER}/open_session?email=#{CGI.escape(email)}&X-YP-AppKey=#{public_key}&X-YP-Signature=#{CGI.escape(signature)}&X-YP-MilliTime=#{@millitime}&X-YP-Int=#{@int}".to_json, nil, nil
     end
     
-    def method_missing(method, *args)
-      id, params, scope = args[0], args[1], args[2]
-      case method.to_s
-      when /^get_(\w+)_by_id$/
-        build_uri("/#{$1}/#{id}.json")
-        build_key(:get, scope)
-        RestClient.get @uri.to_s, http_headers
-      when /^get_(\w+)$/
-        build_uri("/#{$1}.json")
-        build_key(:get, scope)
-        RestClient.get @uri.to_s, http_headers
-      when /^update_(\w+)$/
-        build_uri("/#{$1}/#{id}.json", :query => params.to_query)
-        build_key(:put, scope, params)
-        RestClient.put @uri.to_s, params.to_query, http_headers
-      when /^create_(\w+)$/
-        build_uri("/#{$1}.json", :query => params.to_query)
-        build_key(:post, scope, params)
-        RestClient.post @uri.to_s, params.to_query, http_headers
-      when /^search_(\w+)$/
-        build_uri("/#{$1}/search.json", :query => params.to_query)
-        build_key(:get, scope, params)
-        RestClient.get @uri.to_s, params.to_query, http_headers
-      else
-        super
-      end
+    # Find object such as user, team, link
+    #
+    #   api.find :user, 1
+    #
+    # @param [Symbol] obj
+    # @param [Fixnum] id
+    # @param [Symbol] scope for this request
+    # @return [RestClient::Response]
+    def find(obj, id, scope = :private)
+      build_uri("/#{obj}/#{id}.json")
+      build_key(:get, scope)
+      RestClient.get @uri.to_s, http_headers
+    end
+
+    # Search object such as :user, :team, :link
+    #
+    #   api.search :user, :email => "vincent@yoolink.fr"
+    #
+    # @param [Symbol] obj
+    # @param [Fixnum] params
+    # @param [Symbol] scope for this request
+    # @return [RestClient::Response]
+    def search(obj, params = {}, scope = :private)
+      build_uri("/#{obj}/search.json", :query => params.to_query)
+      build_key(:get, scope, params)
+      RestClient.get @uri.to_s, http_headers
+    end
+
+    # Create object such as :user, :team, :link
+    #
+    #   api.create :group, :name => "Api", :description => "Everything about Api"
+    #
+    # @param [Symbol] obj
+    # @param [Hash] params
+    # @param [Symbol] scope for this request
+    # @return [RestClient::Response]
+    def create(obj, params = {}, scope = :admin)
+      build_uri("/#{obj}.json", :query => params.to_query)
+      build_key(:post, scope, params)
+      RestClient.post @uri.to_s, params.to_query, http_headers
+    end
+
+    # Update object such as :user, :team, :link
+    #
+    #   api.update :group, 1, :description => "Everything about Api and more..."
+    #
+    # @param [Symbol] obj
+    # @param [Fixnum] id
+    # @param [Hash] params
+    # @param [Symbol] scope for this request
+    # @return [RestClient::Response]
+    def update(obj, id, params = {}, scope = :admin)
+      build_uri("/#{obj}/#{id}.json", :query => params.to_query)
+      build_key(:put, scope, params)
+      RestClient.put @uri.to_s, params.to_query, http_headers
     end
     
     private
